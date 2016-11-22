4df6ef93b818291811d06a2899de1cb7df7710c5
LanKuDot <airlanser@gmail.com>
2016-11-22 22:38:46 +0800

    Add the test code for RFID

---
+++ test/test-RFID.ino
@@ -0,0 +1,44 @@
+#include <SPI.h>
+#include <RFID.h>
+
+#define SPI_MOSI 11
+#define SPI_MISO 12
+#define SPI_SCLK 13
+#define SPI_SS   10
+#define MFRC522_RSTPD 9
+
+RFID rfid(SPI_SS, MFRC522_RSTPD);
+
+void setup()
+{
+	SPI.begin();
+	SPI.beginTransaction(SPISettings(10000000L, MSBFIRST, SPI_MODE3));
+
+	rfid.begin();
+
+	Serial.begin(9600);
+	while (!Serial)
+		;
+	Serial.println("ON");
+}
+
+static uint8_t status;
+static uint16_t card_type;
+static uint8_t sn[MAXRLEN], snBytes;
+
+void loop()
+{
+	delay(500);
+	if ((status = rfid.findTag(&card_type)) == STATUS_OK) {
+		Serial.print("OK! ");
+		Serial.println(card_type);
+		if ((status = rfid.readTagSN(sn, &snBytes)) == STATUS_OK) {
+			for (int i = 0; i < snBytes; ++i)
+				Serial.print(sn[i], HEX);
+			Serial.println();
+		}
+	} else {
+		Serial.print("Sad... Error ID: ");
+		Serial.println(status);
+	}
+}
