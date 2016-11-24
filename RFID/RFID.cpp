143884ef9511535a3b8bf0c429c9def7875e1c76
LanKuDot <airlanser@gmail.com>
2016-11-24 15:17:49 +0800

    RFID: Omit the namespace of base class in func call

--- RFID/RFID.cpp
+++ RFID/RFID.cpp
@@ -11,7 +11,7 @@
 uint8_t RFID::findTag(uint16_t *card_type)
 {
 	uint8_t status;
-	if ((status = MFRC522::piccRequest(PICC_REQIDL, _buff)) == STATUS_OK) {
+	if ((status = piccRequest(PICC_REQIDL, _buff)) == STATUS_OK) {
 		*card_type = (uint16_t)(_buff[0] << 8 | _buff[1]);
 	}
 	return status;
@@ -19,16 +19,16 @@ uint8_t RFID::findTag(uint16_t *card_type)
 
 uint8_t RFID::readTagSN(uint8_t *sn, uint8_t *snBytes)
 {
-	if (MFRC522::piccAnticoll(PICC_CASCADE_Lv1, _buff) != STATUS_OK) return STATUS_ERROR;
-	if (MFRC522::piccSelect(PICC_CASCADE_Lv1, _buff) != STATUS_OK) return STATUS_ERROR;
+	if (piccAnticoll(PICC_CASCADE_Lv1, _buff) != STATUS_OK) return STATUS_ERROR;
+	if (piccSelect(PICC_CASCADE_Lv1, _buff) != STATUS_OK) return STATUS_ERROR;
 	if (_buff[0] == 0x88) {
 		memcpy(sn, &_buff[1], 3);
-		if (MFRC522::piccAnticoll(PICC_CASCADE_Lv2, _buff) != STATUS_OK) return STATUS_ERROR;
-		if (MFRC522::piccSelect(PICC_CASCADE_Lv2, _buff) != STATUS_OK) return STATUS_ERROR;
+		if (piccAnticoll(PICC_CASCADE_Lv2, _buff) != STATUS_OK) return STATUS_ERROR;
+		if (piccSelect(PICC_CASCADE_Lv2, _buff) != STATUS_OK) return STATUS_ERROR;
 		if (_buff[0] = 0x88) {
 			memcpy(sn + 3, &_buff[1], 3);
-			if (MFRC522::piccAnticoll(PICC_CASCADE_Lv3, _buff) != STATUS_OK) return STATUS_ERROR;
-			if (MFRC522::piccSelect(PICC_CASCADE_Lv3, _buff) != STATUS_OK) return STATUS_ERROR;
+			if (piccAnticoll(PICC_CASCADE_Lv3, _buff) != STATUS_OK) return STATUS_ERROR;
+			if (piccSelect(PICC_CASCADE_Lv3, _buff) != STATUS_OK) return STATUS_ERROR;
 			memcpy(sn + 6, _buff, 3);
 			*snBytes = 10;
 		} else {
