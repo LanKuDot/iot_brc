ecbfcdfc735c95374bb1abb6432195d2eac2b036
LanKuDot <airlanser@gmail.com>
2016-12-06 21:36:11 +0800

    KSM111_ESP8266: Add comment to example SetBaudrate

--- KSM111_ESP8266/examples/SetBaudrate/SetBaudrate.ino
+++ KSM111_ESP8266/examples/SetBaudrate/SetBaudrate.ino
@@ -1,12 +1,25 @@
+/* Change the default baudrate of the module.
+ * For the first time changing to the different baudrate
+ * may get "Failed" because of garbled characters.
+ * Change the "origBaudrate" to the same as "newBaudrate"
+ * and then run again. You would receive "OK" message.
+ */
 #include <KSM111_ESP8266.h>
 
-#define UART_RX 10
-#define UART_TX 2
+#define UNO
+
+#ifdef UNO
+ #define UART_RX 3
+ #define UART_TX 2
+#else
+ #define UART_RX 10
+ #define UART_TX 2
+#endif
 
 #define origBaudrate 115200
 #define newBaudrate 9600
 
-WIFI wifi(UART_RX, UART_TX);
+KSM111_ESP8266 wifi(UART_RX, UART_TX);
 
 void setup()
 {
